704f8ec0ec73f708aabd526ba8632f45c26bc6a1
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-12-04 14:14:05 +0100

    continuation

--- main.js
+++ main.js
@@ -9,3 +9,30 @@ class EventTicket {
     this.time = time;
   }
 }
+
+
+function main() {
+  console.log("välkommen till event biljetter!");
+  bo = true;
+
+  while (bo) {
+    console.log("Vad vill du göra?");
+    console.log("1: skapa konto");
+    console.log("2: logga in");
+    console.log("3: avsluta");
+    choice = parseInt(prompt());
+
+    switch (choice) {
+      case 1:
+
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      default:
+        break;
+    }
+  }
+
+}
