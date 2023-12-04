f29fbca75d65a3b09b5fec9439c7a57d6079f2c6
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-12-04 22:15:10 +0100

    added some robustness

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const data1 = fs.readFileSync('data.json');
 const data = JSON.parse(data1);
 const users1 = fs.readFileSync('users.json');
 const users = JSON.parse(users1);
+let currentUser;
+let loggedIn = false;
 
 class EventTicket {
   constructor(id, name, price, time) {
@@ -23,21 +25,10 @@ class User {
     this.tickets = [];
   }
 
-  get username() {
-    return this.username;
-  }
-
-  get password() {
-    return this.password;
-  }
-
-  get role() {
-    return this.role;
-  }
-
   buyTicket(event) {
+    console.log(`Tickets before buying: ${this.tickets.length}`);
     this.tickets.push(event);
-    console.log(`Ticket bought for event "${event.name}" by user "${this.username}".`);
+    console.log(`Tickets after buying: ${this.tickets.length}`);
   }
 
   viewTickets() {
@@ -48,36 +39,42 @@ class User {
   }
 }
 
-function loadFromJson(filePath) {
-  dataContent = JSON.parse(data);
-  return dataContent;
-
-}
-
-function saveToJson(data, filePath) {
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-}
-
 function createAccount() {
   const userName = prompt("Användarnamn: ");
   const password = prompt("Lösenord: ", { hideEchoBack: true });
-  const role = prompt("Är du administratör eller köpare? svara med 'a' för administratör och 'k' för köpare");
+  const role = prompt("Är du administratör eller köpare? svara med 'a' för administratör och 'k' för köpare").toLowerCase();
+  if (role != "a" || role != "k") {
+    b = true;
+    while (b) {
+      console.log("Du har matat in en felaktig roll");
+      const role = prompt("Är du administratör eller köpare? svara med 'a' för administratör och 'k' för köpare").toLowerCase();
+      if (role == "a" || role == "k") {
+        b = false;
+      }
+    }
+  }
   const user = new User(users.length + 1, userName, password, role)
   console.log("Ditt Konto har skapats");
 
   users.push(user);
+  currentUser = user
+  fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf8');
+  console.clear();
+  return user
 }
 
 function logIn() {
   const username = prompt('Användarnamn: ');
   const password = prompt('Lösenord: ');
+  const userData = users.find(u => u.username === username && u.password === password);
 
-  const user = users.find(u => u.username === username && u.password === password);
-
-  if (!user) {
+  if (!userData) {
     console.log('Användaren hittades inte. Vänligen försök igen.');
     return null;
   } else {
+    const user = new User(userData.id, userData.username, userData.password, userData.role);
+    user.tickets = userData.tickets
+    loggedIn = true;
     return user;
   }
 }
@@ -87,28 +84,43 @@ function main() {
   let bo = true;
 
   while (bo) {
+
     console.log("Vad vill du göra?");
     console.log("1: skapa konto");
     console.log("2: logga in");
     console.log("3: avsluta");
+
     let choice
     try {
       choice = parseInt(prompt());
     } catch (error) {
       error.message;
     }
+
     switch (choice) {
       case 1:
-        createAccount();
+        currentUser = createAccount();
         break;
       case 2:
-        let currentUser = logIn()
-        if (currentUser) {
-          console.log("4: lägg till biljetter");
-          console.log("5: se mina biljetter");
-        }
-        else {
-          console.log("4: se användare som har köpt biljetter");
+        currentUser = logIn()
+        if (currentUser && loggedIn) {
+          console.clear();
+          if (currentUser.role === "k") {
+            console.log("Vad vill du göra?");
+            console.log("1: skapa konto");
+            console.log("2: logga in");
+            console.log("3: avsluta");
+            console.log("4: lägg till biljetter");
+            console.log("5: se mina biljetter");
+            console.log("6: köp biljetter");
+          }
+          else {
+            console.log("Vad vill du göra?");
+            console.log("1: skapa konto");
+            console.log("2: logga in");
+            console.log("3: avsluta");
+            console.log("4: se användare som har köpt biljetter");
+          }
         }
         break;
       case 3:
@@ -117,19 +129,63 @@ function main() {
         fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf8');
         break;
       case 4:
-        if (currentUser.role === "k") {
+        if (loggedIn && currentUser.role === "k") {
           const eventName = prompt("vad heter eventet du vil lägga till? ");
-          const eventPrice = parseInt(prompt("Hur mycket ska en biljett kosta?"));
+          const eventPrice = parseInt(prompt("Ange biljett kostnad"));
+          if (Number.isInteger(eventPrice)) {
+            b = true;
+            while (b) {
+              console.log("Du har matat in en felaktig roll");
+              const eventPrice = parseInt(prompt("Ange biljett kostnad"));
+              if (Number.isInteger(eventPrice)) {
+                b = false;
+              }
+            }
+          }
+          const eventTime = Date(prompt("Tiden eventet sker"));
 
+          const newEvent = new EventTicket(data.length + 1, eventName, eventPrice, eventTime)
+          currentUser.
+            data.push(newEvent);
+          fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
         }
+        else if (loggedIn) {
+          console.log(currentUser);
+          currentUser.viewTickets();
+        }
+        else {
+          console.log("användaren finns inte");
+        }
+
         break;
       case 5:
+        if (loggedIn && currentUser.role === 'k') {
+          currentUser.viewTickets()
+        }
+        break;
+      case 6:
+        if (loggedIn && currentUser.role === "k") {
+          data.forEach(event => {
+            console.log(`ID: ${event.id}, Event: ${event.name}, Price: ${event.price}, Time: ${event.time}\n`)
+          });
+          const eventId = parseInt(prompt("Enter the ID of the event you want to buy a ticket for: "));
+          const selectedEvent = data.find(event => event.id === eventId);
+
+          if (selectedEvent) {
+            currentUser.buyTicket(selectedEvent);
+            fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
+            fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf8');
+          } else {
+            console.log("Event not found.");
+          }
+        } else {
+          console.log("Only buyers can purchase tickets. You are not a buyer.");
+        }
         break;
       default:
         break;
     }
   }
-
 }
 
 main();
