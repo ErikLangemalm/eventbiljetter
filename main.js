bd43e2082880baac9fa2157bb763fed0e1848271
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-12-04 17:21:22 +0100

    added data.json

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const prompt = require('prompt-sync')();
 
+
 class EventTicket {
   constructor(id, name, price, time) {
     this.id = id;
@@ -10,25 +11,66 @@ class EventTicket {
   }
 }
 
+class User {
+  constructor(id, username, password, role) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.tickets = [];
+  }
+
+  buyTicket(event) {
+    this.tickets.push(event);
+    console.log(`Ticket bought for event "${event.name}" by user "${this.username}".`);
+  }
+
+  viewTickets() {
+    console.log(`Tickets bought by user "${this.username}":`);
+    this.tickets.forEach(event => {
+      console.log(`Event: ${event.name}, Price: ${event.price}, Time: ${event.time}`);
+    });
+  }
+}
+/*
+function loadFromJson(filePath) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
+
+}
+
+let events = loadFromJson('data.json');
+let users = loadFromJson('users.json');
+*/
+function createAccount() {
+  const userName = prompt("Användarnamn: ");
+  const pswrd = prompt("Lösenord: ", { hideEchoBack: true });
+  const type = prompt("Är du säljare eller köpare? svara med 's' för säljare och 'k' för köpare");
+}
 
 function main() {
   console.log("välkommen till event biljetter!");
-  bo = true;
+  let bo = true;
 
   while (bo) {
     console.log("Vad vill du göra?");
     console.log("1: skapa konto");
     console.log("2: logga in");
     console.log("3: avsluta");
-    choice = parseInt(prompt());
-
+    let choice
+    try {
+      choice = parseInt(prompt());
+    } catch (error) {
+      error.message;
+    }
     switch (choice) {
       case 1:
-
+        createAccount();
         break;
       case 2:
         break;
       case 3:
+        bo = false;
         break;
       default:
         break;
@@ -36,3 +78,5 @@ function main() {
   }
 
 }
+
+main();
